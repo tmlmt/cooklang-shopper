f719ebb44fd120ee5b5e0051dafb1080c28bc095
Thomas Lamant <tom@tmlmt.com>
2025-11-30 22:07:41 +0100

    style: change secondary color to royal blue / lavender

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   ui: {
     colors: {
       primary: "blue",
-      secondary: "sky",
+      secondary: "royal",
       success: "emerald",
       info: "sky",
       warning: "amber",
