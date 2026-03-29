94d978b1fabf9c7336004196ecf7ecb599ccb2b6
Thomas Lamant <tom@tmlmt.com>
2026-03-29 20:39:12 +0200

    fix(layout): center auth container

--- app/layouts/naked.vue
+++ app/layouts/naked.vue
@@ -1,5 +1,10 @@
 <template>
-  <UContainer class="absolute flex min-h-screen items-center justify-center">
+  <UContainer
+    class="absolute flex min-h-screen items-center justify-center"
+    :ui="{
+      base: 'max-w-none',
+    }"
+  >
     <slot />
   </UContainer>
 </template>
