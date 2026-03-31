32b889d9831adad5f35807d2aeec636a92bc8111
Thomas Lamant <tom@tmlmt.com>
2026-04-01 01:26:26 +0200

    chore: add vue-tsc for typecheck and fix found issues

--- server/utils/validateRecipePath.ts
+++ server/utils/validateRecipePath.ts
@@ -1,5 +1,5 @@
-import nodePath from "node:path";
-import type { H3Event } from "h3";
+import * as nodePath from "node:path";
+import { createError, getRouterParam, type H3Event } from "h3";
 import { validateRecipePath } from "~~/shared/utils/path";
 
 const recipesRoot = nodePath.resolve(process.cwd(), "public", "recipes");
--- shared/utils/path.ts
+++ shared/utils/path.ts
@@ -2,9 +2,6 @@ import { pathRegex } from "../regex";
 
 export function validateRecipePath(path: string) {
   if (!pathRegex.test(path)) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "Invalid recipe path",
-    });
+    throw new Error("Invalid recipe path");
   }
 }
