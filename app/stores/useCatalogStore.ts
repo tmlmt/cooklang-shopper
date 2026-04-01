ceee229eadf1d498903286e55174c45dcdb183cf
Thomas Lamant <tom@tmlmt.com>
2026-04-01 10:06:29 +0200

    chore(useCatalogStore): do not use useFetch in store

--- app/stores/useCatalogStore.ts
+++ app/stores/useCatalogStore.ts
@@ -5,9 +5,9 @@ export const useCatalogStore = defineStore("catalog", () => {
     // only fetch if the catalog is empty
     if (rawCatalog.value) return;
 
-    const { data } = await useFetch<string>("/api/catalog");
-    if (data.value) {
-      rawCatalog.value = data.value;
+    const data = await $fetchWithHeaders<string>("/api/catalog");
+    if (data) {
+      rawCatalog.value = data;
     }
   }
 
